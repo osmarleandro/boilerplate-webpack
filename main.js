d4cbb99c6748966a050a9d358adef924dd2c61c7
Osmar Leandro <osmarleandrods@gmail.com>
2018-09-16 18:20:53 -0300

    Babel configuration

---
+++ main.js
@@ -0,0 +1,30 @@
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(item) {
+    this.data.push(item);
+    console.log(this.data);
+  }
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.user = 'olds';
+  }
+
+  getUser() {
+    return this.user;
+  }
+
+}
+
+const mylist = new TodoList();
+
+document.getElementById('newtodo').onclick = () => {
+  mylist.add('New Todo');
+}
+
+console.log(mylist.getUser());
