6e7d05268f7cfb5ef0411086a82dde8e26c5d8f5
Osmar Leandro <osmarleandrods@gmail.com>
2018-09-21 12:39:43 -0300

    axios config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
-import {
-  sum
-} from './functions';
+import sum from './functions';
 console.log(`The sum is ${sum(1, 2)}`);
 
+
+/***
+ * Async await example
+ */
 const thePromise = () => new Promise((resolve, reject) => {
   setTimeout(() => resolve('OK'), 2000);
 });
@@ -15,5 +17,22 @@ async function executePromise() {
   console.log(await thePromise());
   console.log(await thePromise());
 }
-
 executePromise();
+
+/**
+ * Axios example
+ */
+import axios from 'axios';
+class ServiceApi {
+  static async getGithubUser(username) {
+    return await axios.get(`https://api.github.com/users/${username}`);
+  }
+}
+
+ServiceApi.getGithubUser('osmarleandro')
+  .then(response =>
+    console.log(response)
+  )
+  .catch(error =>
+    console.error(error)
+  )
